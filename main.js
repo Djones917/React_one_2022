13c9e666d8e58fec39030cbf6c3e2da952357779
Derek <nightwing917@hotmail.com>
2022-02-17 19:43:33 -0700

    added good bye

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ class Hello extends React.Component {
     }
 }
 
-ReactDOM.render(<Hello/>, document.getElementById('root'))
+
+// function Bye() {
+//     return <h1>Good Bye!</h1>;
+// }
+
+ReactDOM.render(<Hello/>, document.getElementById('root'));
+// ReactDOM.render(<Bye/>, document.getElementById('root'));
