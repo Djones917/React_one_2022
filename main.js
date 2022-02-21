14c6571dff6ec68f4a1ce616bdc60c3abd5485f2
Derek <nightwing917@hotmail.com>
2022-02-20 20:27:26 -0700

    playing around with conditionals in jsx

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-function getMood() {
-    const moods = ['Horny', 'Hungry', 'Happy', 'Sad', 'God Mode'];
-    return moods[Math.floor(Math.random() * moods.length)];
-}
+// function getMood() {
+//     const moods = ['Horny', 'Hungry', 'Happy', 'Sad', 'God Mode'];
+//     return moods[Math.floor(Math.random() * moods.length)];
+// }
 
-class Hello extends React.Component {
+// class Hello extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello There!</h1>
+//                 <h1>My number is: {18}</h1>
+//                 <h1>My current mood is: {getMood()}</h1>
+//             </div>
+//         );
+//     }
+// }
+
+// // function Bye() {
+// //     return <h1>Good Bye!</h1>;
+// // }
+
+// ReactDOM.render(<Hello/>, document.getElementById('root'));
+// // ReactDOM.render(<Bye/>, document.getElementById('root'));
+
+
+
+
+// Conditionals in JSX
+class NumPicker extends React.Component {
     render() {
-        return (
-            <div>
-                <h1>Hello There!</h1>
-                <h1>My number is: {18}</h1>
-                <h1>My current mood is: {getMood()}</h1>
-            </div>
-        );
+
     }
 }
 
-
-// function Bye() {
-//     return <h1>Good Bye!</h1>;
-// }
-
-ReactDOM.render(<Hello/>, document.getElementById('root'));
-// ReactDOM.render(<Bye/>, document.getElementById('root'));
+ReactDOM.render(<NumPicker />, document.getElementById('root'));
