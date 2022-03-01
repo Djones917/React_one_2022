8a943b2e18a282f6bc69877b128841e4a615d6ee
Derek <nightwing917@hotmail.com>
2022-02-28 20:19:18 -0700

    fixed mistake in index html

--- main.js
+++ main.js
@@ -1,45 +1,11 @@
-// function getMood() {
-//     const moods = ['Horny', 'Hungry', 'Happy', 'Sad', 'God Mode'];
-//     return moods[Math.floor(Math.random() * moods.length)];
-// }
+class App extends React.Component {
+    render() {
+        return (
+        <div>
+          <Hello />
+        </div>
+        )
+    }
+}
 
-// class Hello extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Hello There!</h1>
-//                 <h1>My number is: {18}</h1>
-//                 <h1>My current mood is: {getMood()}</h1>
-//             </div>
-//         );
-//     }
-// }
-
-// // function Bye() {
-// //     return <h1>Good Bye!</h1>;
-// // }
-
-// ReactDOM.render(<Hello/>, document.getElementById('root'));
-// // ReactDOM.render(<Bye/>, document.getElementById('root'));
-
-
-
-// Get Number
-// function getNum() {
-//   return Math.floor(Math.random() * 10 + 1);
-// }
-
-// // Conditionals in JSX
-// class NumPicker extends React.Component {
-//     render() { 
-//             const num = getNum();         
-//             return (
-//               <div>
-//               <h1>Your Number is: {num}</h1>
-//               <p>{num === 7 ? 'Congrats' : 'Try Next Time'}</p>
-//               </div>              
-//           )         
-//     }
-// }
-
-// ReactDOM.render(<NumPicker />, document.getElementById('root'));
+ ReactDOM.render(<App />, document.getElementById('root'));
